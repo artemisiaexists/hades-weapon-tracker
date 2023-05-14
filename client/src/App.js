27c577bb94c7a9c67df400ef5c97f869dfc96c65
em <ertrzupek@gmail.com>
2023-05-14 17:48:58 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,81 @@
+import React from "react";
+import './main.css';
+//https://hades-weapons-server.onrender.com/weapons
+
+const Weapon = (props) => {
+    const weapon = props.i;
+    const progress = [props.progress[0], props.progress[1], props.progress[2]]
+    const numColor = (-255/59)*parseInt(props.heat) + 255;
+    const disableNewlines = (event) => {
+        const keyCode = event.keyCode || event.which
+        if (keyCode === 13) {
+            event.returnValue = false
+            if (event.preventDefault) event.preventDefault()
+            props.enter(event.target.innerText, weapon, event.target.className.split(" ")[1])
+        }
+    }
+    return(
+        <tr>
+            <td key = {5} className="weaponimgcont"><div className="weaponimgcell"><a href = {props.url} target="_blank" rel="noreferrer"><img src={require(`./imgs/${props.name}.png`)} className="weaponimgs" alt = {props.name}/></a></div></td>
+            <td key = {6} style={{color: `rgb(255, ${numColor}, 0)`}}><span className="form" role="textbox" contentEditable={true} onKeyPress={disableNewlines}>{props.heat}</span></td>
+            {progress.map((level, i) => { if(level) {return(<td key = {i}><input type="checkbox" className="checkbox" onChange={props.change} checked/></td>)} else { return(<td key = {i}><input type="checkbox" id = {`${weapon} ${i}`} className="checkbox" onChange={props.change}/></td>)}})}
+            <td key = {3}><input type="checkbox" id = {`${weapon} 3`} className="checkbox" onChange={props.change}/></td>
+        </tr>
+    )
+}
+
+const App = () => {
+    const [data, setData] = React.useState([]);
+    const levels = ["tartarus", "asphodel", "elysium", "styx"];
+    React.useEffect(() => {
+        fetch("http://localhost:3001/weapons")
+        .then(res => res.json())
+        .then(json => setData(json.weapons));
+    }, []);
+    const changeValue = (event) => {
+        const weapon = event.target.id.split(" ")[0];
+        const level = event.target.id.split(" ")[1];
+        if(parseInt(level) === 3) event.target.checked = false;
+        fetch("http://localhost:3001/weapons", {
+            method: "post",
+            headers: {
+                "weapon": weapon,
+                "level": level, 
+                "heat": -1
+            },
+        })
+            .then(res => res.json())
+            .then(json => setData(json.weapons))
+    }
+    function enterValue(newHeat, weapon, level) {
+        fetch("http://localhost:3001/weapons", {
+            method: "post",
+            headers: {
+                "weapon": weapon,
+                "level": level, 
+                "heat": newHeat
+            },
+        })
+            .then(res => res.json())
+            .then(json => setData(json.weapons))
+    }
+    return (
+        <div id="container">
+            <p style={{textAlign: "center"}}>Weapon Heat Tracker</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th key ={6}></th>
+                        <th key ={5}><img src={require(`./imgs/heat.png`)} id="heatimg" alt = "heat"/></th>
+                        {levels.map((level, i) => {return(<th key ={i}><img src={require(`./imgs/${i}.png`)} className="levelimgs" alt = {level}/></th>)})}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item,i) => {return(<Weapon i = {i} key = {i} name = {item.name} heat = {item.heat} progress = {item.progress} change = {changeValue} enter = {enterValue} url = {item.url}/>)})}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const fs = require("fs")
+
+let data = [];
+fs.readFile(
+      "server/data.json",
+      (err, res) => {
+            if(!err){
+                data = JSON.parse(res);
+            } else {
+                console.log(err)
+            }
+      }
+)
+
+app.get("/weapons", (req, res) => {
+    const weapons = data.weapons;
+    res.send({weapons})
+});
+
+app.post("/weapons", (req, res) => {
+    const weaponI = req.headers.weapon;
+    const level = req.headers.level;
+    dataToChange = data.weapons[weaponI];
+    if(req.headers.heat < 0) {
+        if(parseInt(level) === 3) {
+            console.log("here")
+            for(var i = 0; i < 4; i++) {
+                dataToChange.progress[i] = false;
+            }
+            dataToChange.heat += 1;
+        } else {
+            for(var i = level; i > -1; i--) {
+                dataToChange.progress[i] = true;
+            }
+        }
+    } else {
+        dataToChange.heat = req.headers.heat;
+        for(var i = 0; i < 4; i++) {
+            dataToChange.progress[i] = false;
+        }
+    }
+    data.weapons[weaponI] = dataToChange;
+    fs.writeFile(
+        "server/data.json",
+        JSON.stringify(data, null, 2),
+        (err) => {console.log(err)})
+    const weapons = data.weapons;
+    res.send({weapons})
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
